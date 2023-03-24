badefcd90b195c1807962477f0ee05eeb6e40d2b
Naim <skylike2904@gmail.com>
2023-03-24 17:57:42 +0600

    up

---
+++ src/pages/about/data/IntroSectionData.js
@@ -0,0 +1,22 @@
+export const IntroSectionData = {
+  left: {
+    heading: "Who Am I?",
+    description:
+      "Hello, I'm Naimur, a Full Stack Developer. I'm a very consistent learner. I work with React JS for frontend & FastAPI in backend. I've experience using both SQL & No SQL databases. But mostly I've worked with PostgreSQL. Also I really love working with machine learning. I love & try learning something new everyday.",
+  },
+  right: {
+    top: {
+      totalExpYears: "2",
+      totalToolsUsed: "10",
+    },
+
+    bottom: {
+      name: "Naimur Rahman",
+      languages: "ENG/BNG/JPN",
+      freelancing: "Available",
+      location: "Gazipur, Bangladesh",
+      phone: "+8801843034859",
+      email: "prof.naimur29@gmail.com",
+    },
+  },
+};
---
+++ src/pages/about/data/TechStacks.js
@@ -0,0 +1,152 @@
+// importing language icons:
+import HTMLIcon from "../../../assets/tech-icons/html_icon.svg";
+import CSSIcon from "../../../assets/tech-icons/css_icon.svg";
+import SassIcon from "../../../assets/tech-icons/sass_icon.svg";
+import JavaScriptIcon from "../../../assets/tech-icons/javascript_icon.svg";
+import TypeScriptIcon from "../../../assets/tech-icons/typescript_icon.svg";
+import PythonIcon from "../../../assets/tech-icons/python_icon.svg";
+import CIcon from "../../../assets/tech-icons/c_icon.svg";
+import CPPIcon from "../../../assets/tech-icons/cpp_icon.svg";
+
+// importing Frameworks Icons:
+import ReactJSIcon from "../../../assets/tech-icons/react_icon.svg";
+import MaterialIcon from "../../../assets/tech-icons/mui_icon.svg";
+import TailwindIcon from "../../../assets/tech-icons/tailwind_icon.svg";
+import FlowbiteIcon from "../../../assets/tech-icons/flowbite_icon.svg";
+import NodeJSIcon from "../../../assets/tech-icons/node_icon.svg";
+import FastAPIIcon from "../../../assets/tech-icons/fastapi_icon.svg";
+
+// importing Tools Icons:
+import GitIcon from "../../../assets/tech-icons/git_icon.svg";
+import GitHubIcon from "../../../assets/tech-icons/github_icon.svg";
+import NetlifyIcon from "../../../assets/tech-icons/netlify_icon.svg";
+import LinuxIcon from "../../../assets/tech-icons/linux_icon.svg";
+import TerminalIcon from "../../../assets/tech-icons/terminal_icon.svg";
+import HerokuIcon from "../../../assets/tech-icons/heroku_icon.svg";
+import PostgreSQLIcon from "../../../assets/tech-icons/postgresql_icon.svg";
+import MongoDBIcon from "../../../assets/tech-icons/mongodb_icon.svg";
+import VSCodeIcon from "../../../assets/tech-icons/vscode_icon.svg";
+
+export const Languages = [
+  {
+    img: HTMLIcon,
+    label: "HTML",
+    bgColor: "#e44f2633",
+  },
+  {
+    img: CSSIcon,
+    label: "CSS",
+    bgColor: "#1572b633",
+  },
+  {
+    img: SassIcon,
+    label: "Sass",
+    bgColor: "#c693",
+  },
+  {
+    img: JavaScriptIcon,
+    label: "JavaScript",
+    bgColor: "#f7df1e33",
+  },
+  {
+    img: TypeScriptIcon,
+    label: "TypeScript",
+    bgColor: "#007acc33",
+  },
+  {
+    img: PythonIcon,
+    label: "Python",
+    bgColor: "#ffc85133",
+  },
+  {
+    img: CIcon,
+    label: "C Lang",
+    bgColor: "#03599C33",
+  },
+  {
+    img: CPPIcon,
+    label: "C++",
+    bgColor: "#984c9333",
+  },
+];
+
+export const Frameworks = [
+  {
+    img: ReactJSIcon,
+    label: "ReactJS",
+    bgColor: "#00d8ff33",
+  },
+  {
+    img: MaterialIcon,
+    label: "Material UI",
+    bgColor: "#fff3",
+  },
+  {
+    img: TailwindIcon,
+    label: "Tailwind CSS",
+    bgColor: "#44a8b333",
+  },
+  {
+    img: FlowbiteIcon,
+    label: "Flowbite",
+    bgColor: "#1724C933",
+  },
+  {
+    img: NodeJSIcon,
+    label: "NodeJS",
+    bgColor: "#83cd2933",
+  },
+  {
+    img: FastAPIIcon,
+    label: "FastAPI",
+    bgColor: "",
+  },
+];
+
+export const Tools = [
+  {
+    img: GitIcon,
+    label: "Git",
+    bgColor: "#dd4c3533",
+  },
+  {
+    img: GitHubIcon,
+    label: "GitHub",
+    bgColor: "#fff3",
+  },
+  {
+    img: NetlifyIcon,
+    label: "Netlify",
+    bgColor: "#21c6b733",
+  },
+  {
+    img: LinuxIcon,
+    label: "Linux",
+    bgColor: "#FFCD0A33",
+  },
+  {
+    img: TerminalIcon,
+    label: "Terminal Shell",
+    bgColor: "#fff3",
+  },
+  {
+    img: HerokuIcon,
+    label: "Heroku",
+    bgColor: "#4093",
+  },
+  {
+    img: PostgreSQLIcon,
+    label: "PostgreSQL",
+    bgColor: "#33679133",
+  },
+  {
+    img: MongoDBIcon,
+    label: "MongoDB",
+    bgColor: "#59963633",
+  },
+  {
+    img: VSCodeIcon,
+    label: "Visual Studio Code",
+    bgColor: "#0065a933",
+  },
+];
