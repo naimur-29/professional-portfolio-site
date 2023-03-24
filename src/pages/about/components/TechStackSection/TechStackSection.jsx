a54bd09457c5f9bb6646bebc4ad859a18df9cc90
Naim <skylike2904@gmail.com>
2023-03-24 12:35:37 +0600

    added style to the techstack section of about page

--- src/pages/about/components/TechStackSection/TechStackSection.jsx
+++ src/pages/about/components/TechStackSection/TechStackSection.jsx
@@ -9,15 +9,19 @@ import { Languages } from "../../data/TechStacks";
 const TechStackSection = () => {
   return (
     <section className="tech-stack-section-container">
-      <h3 className="title">
+      <h3 className="heading">
         Here are the technologies I've been working with recently
       </h3>
 
       <div className="main-container">
         <div className="content-container">
           <h3 className="title">Languages</h3>
-          {Languages?.map(({ img, label }, key) => (
-            <div className="item-container" key={key}>
+          {Languages?.map(({ img, label, bgColor }, key) => (
+            <div
+              className="item-container"
+              key={key}
+              style={{ backgroundColor: bgColor }}
+            >
               <img src={img} alt={`${label} icon`} className="icon" />
               <span className="label">{label}</span>
             </div>
@@ -25,9 +29,13 @@ const TechStackSection = () => {
         </div>
 
         <div className="content-container">
-          <h3 className="title">Languages</h3>
-          {Languages?.map(({ img, label }, key) => (
-            <div className="item-container" key={key}>
+          <h3 className="title">Frameworks</h3>
+          {Languages?.map(({ img, label, bgColor }, key) => (
+            <div
+              className="item-container"
+              key={key}
+              style={{ backgroundColor: bgColor }}
+            >
               <img src={img} alt={`${label} icon`} className="icon" />
               <span className="label">{label}</span>
             </div>
@@ -35,9 +43,13 @@ const TechStackSection = () => {
         </div>
 
         <div className="content-container">
-          <h3 className="title">Languages</h3>
-          {Languages?.map(({ img, label }, key) => (
-            <div className="item-container" key={key}>
+          <h3 className="title">Tools</h3>
+          {Languages?.map(({ img, label, bgColor }, key) => (
+            <div
+              className="item-container"
+              key={key}
+              style={{ backgroundColor: bgColor }}
+            >
               <img src={img} alt={`${label} icon`} className="icon" />
               <span className="label">{label}</span>
             </div>
